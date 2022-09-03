ae94b4c4229af9fe186cad4785ad72df09873b18
weijunliang23 <929314787@qq.com>
2022-09-03 21:06:30 +0800

    feat: first-commit

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import './App.css'
+import { Link, Route, Routes, BrowserRouter } from 'react-router-dom'
+
+// Auto generates routes from files under ./pages
+// https://vitejs.dev/guide/features.html#glob-import
+interface TypePages {
+  default: () => void,
+  [K: string]: any,
+}
+// @ts-ignore
+const pages: TypePages = import.meta.glob('./pages/*.tsx', { eager: true })
+
+const routes = Object.keys(pages).map((path) => {
+  const name = path.match(/\.\/pages\/(.*)\.tsx$/)![1]
+  return {
+    name,
+    path: name === 'Home' ? '/' : `/${name.toLowerCase()}`,
+    component: pages[path]?.default || null
+  }
+})
+
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            {routes.map(({ name, path }) => {
+              return (
+                <li key={path}>
+                  <Link to={path}>{name}</Link>
+                </li>
+              )
+            })}
+          </ul>
+        </nav>
+        <Routes>
+          {routes.map(({ path, component: RouteComp }) => {
+            return <Route key={path} path={path} element={<RouteComp />}></Route>
+          })}
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
