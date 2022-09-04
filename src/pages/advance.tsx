c91b9fd3037005f4c8b8350ae773e8d2ea2f3602
weijunliang23 <929314787@qq.com>
2022-09-04 22:30:03 +0800

    test: first-exp-tailwind

--- src/pages/advance.tsx
+++ src/pages/advance.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Advance = () => {
   return (
-    <div>Advance</div>
+    <div className='text-center focus-within:text-left'>
+      1
+      <div className='hover:bg-pink-400 focus:border-blue-50' contentEditable>
+        我的热情是似火！
+      </div>
+      <div id="about" className="target:text-right">
+        598
+      </div>
+      <div>
+        <span className='text-rose-500'>1111</span>
+      </div>
+    </div>
   )
 }
 
