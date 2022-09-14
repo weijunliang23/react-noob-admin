fb8f8fa999d91491cc9f7c4098fc3450b50a5c00
weijunliang23 <929314787@qq.com>
2022-09-14 10:36:40 +0800

    feat: 类修饰符属性

---
+++ tests/1.ts
@@ -0,0 +1,29 @@
+class Test {
+  static name1: string
+  public fc1(): void {
+    console.log(111)
+  }
+  protected fc2(): void {
+    console.log(2)
+  }
+  private fc3(): void {
+    console.log(3)
+    this.name1 = "2" // 只能通过类访问Test.name1
+  }
+}
+
+abstract class TestSon extends Test {
+  constructor() {
+    super()
+  }
+  fc4() {
+    this.fc2()
+    this.fc3() // 为私有方法只能在Test类里访问
+  }
+}
+
+const a = new TestSon() // TestSon为抽象类不能创建实例
+a.fc1()
+a.fc2()
+const b = new Test()
+b.fc3() // 只能在类和子类里访问
