9282fda7f6dfa517203969fee2ffeef76da6e00a
weijunliang23 <929314787@qq.com>
2022-09-18 15:18:06 +0800

    feat: 配置代理

---
+++ serve/app.js
@@ -0,0 +1,12 @@
+// const Controller = require("egg").Controller
+
+// class HomeController extends Controller {
+//   async index() {
+//     this.ctx.response = "Hello world"
+//   }
+// }
+// // app/router.js
+// ;(app) => {
+//   const { router, controller } = app
+//   router.get("/ggsmd", controller.home.index)
+// }
