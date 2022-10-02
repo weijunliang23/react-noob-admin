ddd5e41a128703fee24d73c68a21affe9388c673
weijunliang23 <929314787@qq.com>
2022-10-02 08:21:50 +0800

    feat: vue/react大屏幕scale适配

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import "../styles/home.css"
 const Home = () => {
   return (
-    <div>首页</div>
+    <div>
+      <div className='bg h-52 w-52 overflow-hidden'>
+        <div className='w-9 h-24 backdrop-blur-md bg-gray-100 mx-auto mt-9 ff'>1</div>
+      </div>
+
+    </div>
   )
 }
 
